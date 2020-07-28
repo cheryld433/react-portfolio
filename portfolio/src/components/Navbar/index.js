65e4ad1110928f746ddedee15cd0f43011f510dc
Cheryl Daniels <cheryld433@gmail.com>
2020-07-28 14:54:17 -0400

    added return function to home.js

--- portfolio/src/components/Navbar/index.js
+++ portfolio/src/components/Navbar/index.js
@@ -1,6 +1,19 @@
 import React from "react";
+import {Link } from "react-router-dom";
+// The primary way to allow users to navigate around your application. 
+{/* <Link> will render a fully accessible anchor tag with the proper href. */}
 
 const Navbar = () => {
+    return(
+        <div className="nav-wrapper">
+            <ul className="navbar">
+                <li className="nav-item">
+                    <Link to="/" 
+                </li>
+            </ul>
+        </div>
+      
+    )
 
 }
 
--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -4,18 +4,51 @@ import { Col, Row } from "react-bootstrap";
 // import home.css
 // import resume 
 
+// LOOK INTO REACT-REVEAL FOR ANIMATION OF THE WEBPAGE!!!!!!!
 const Home = () => {
-  // create the home page: 
-//   return()
-  // container
-  // navbar (jumbotron / tabs?)
-  // github icon/link, linkedIn icon/link resume/link (look into ability to download)
-  // footer
+   return(
+       <div className="home-container">
+           <div className="container">
+               <div className="home-wrapper">
+                   <div className="row">
+                       <div className="col-12">
+                           <div className="row name-title jumbotron-4 justify-content-center">
+                               {/* possible reveal affect here */}
+                               <h1 className="display-">Cheryl Daniels</h1>
+                           </div>
+                           {/* To create a flexbox container and to transform direct children 
+                           into flex items, use the d-flex class: */}
+                           <div className="row subtitle d=flex justify-content-center">
+                               <h5>What goes here?</h5>
+                           </div>
+                           <Row className="d-flex justify-content-center">
+                               {/* links to icons for github, linkedIn and my Resume */}
+                               <Row className="row-icon-row">
+                                   <Col size="xs"><a href="https://github.com/cheryld433"><i className="fab fa-github fa-2x"></i></a></Col>
+                                   <Col size="xs"><a href="https://www.linkedin.com/in/cheryl-daniels-17171619a/"></a><i className="fab fa-linkedin fa-2x"></i></Col>
+                                   {/* need to add resume file  */}
+                                   {/* <Col size="xs"><a href={Resume} download><i className="fas fa-file fa-2x"></i></a></Col> */}
+                               </Row>
+                           </Row>
+                           {/* may use react-reveal on navbar */}
+                           {/* <div lclassName="row-justify-content-center about-Navbar">
+                               <Navbar />
+                           </div> */}
+                           {/* footer */}
+                           <div className="row">
+                               <p className="d-flex justify-content-center footer">&copy;Copyright 2020 Cheryl Daniels</p>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
 
-
-
-  //
-
+  )
 }
+// need to attach resume;
+// need to create Navbar;
+// need to attach css;
+
 
 export default Home;
