639bf1e718e026f486c80352e30bf67a2fef1856
Cheryl Daniels <cheryld433@gmail.com>
2020-08-01 22:17:19 -0400

    Updates

---
+++ precache-manifest.0b8d9fc0c5746b9c034375c16f8c3eb4.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "debc83d448484d69051623c1032e1ac4",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "18bc19952cad4ae2b4bc",
+    "url": "/react-portfolio/static/css/2.304e65a6.chunk.css"
+  },
+  {
+    "revision": "caa2c02156963886a1c5",
+    "url": "/react-portfolio/static/css/main.8c32f116.chunk.css"
+  },
+  {
+    "revision": "18bc19952cad4ae2b4bc",
+    "url": "/react-portfolio/static/js/2.2e2cf700.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.2e2cf700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caa2c02156963886a1c5",
+    "url": "/react-portfolio/static/js/main.a50da0ad.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "1dc17bfcbee886a7995ca7e92c5ce64f",
+    "url": "/react-portfolio/static/media/Homemade.1dc17bfc.png"
+  },
+  {
+    "revision": "380ea3f972f582a665a221fc5ba7934d",
+    "url": "/react-portfolio/static/media/Me.380ea3f9.jpeg"
+  },
+  {
+    "revision": "7e772f4ed0853a2ceba3e990a32ac1e4",
+    "url": "/react-portfolio/static/media/budget-tracker.7e772f4e.png"
+  },
+  {
+    "revision": "612b1486578a39accb7bae43a640a663",
+    "url": "/react-portfolio/static/media/burger.612b1486.png"
+  },
+  {
+    "revision": "36dee2f0c565ea347140e21884366781",
+    "url": "/react-portfolio/static/media/cheryl_daniels.36dee2f0.pdf"
+  },
+  {
+    "revision": "6fad98cb3bf25450712baba4fd9607f6",
+    "url": "/react-portfolio/static/media/contact-background.6fad98cb.png"
+  },
+  {
+    "revision": "3ad2a364b3103d64542307172250a2d0",
+    "url": "/react-portfolio/static/media/homeBackground1.3ad2a364.png"
+  },
+  {
+    "revision": "477918f3557d7026a5065ae9dc32c659",
+    "url": "/react-portfolio/static/media/notetaker.477918f3.png"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/react-portfolio/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/react-portfolio/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/react-portfolio/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/react-portfolio/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  },
+  {
+    "revision": "30ab80cb91fc3597580d38801a1fcf69",
+    "url": "/react-portfolio/static/media/react-directory.30ab80cb.png"
+  },
+  {
+    "revision": "bcbaa58d5e4bcaaa9ff993e02e9012b2",
+    "url": "/react-portfolio/static/media/red-planet.bcbaa58d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cheryld433/react-portfolio.git/precache-manifest.8f0c245cc1055f3415ff0188aeabd679.js"
+  "/react-portfolio/precache-manifest.0b8d9fc0c5746b9c034375c16f8c3eb4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cheryld433/react-portfolio.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
